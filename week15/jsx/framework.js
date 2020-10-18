500535ee6805ffda763507b73a63cde22b7c34eb
Yolo <kuroikenshi@sina.com>
2020-10-19 00:02:30 +0800

    更新作业

--- week15/jsx/framework.js
+++ week15/jsx/framework.js
@@ -1,50 +1,70 @@
-export function createElement(type, attributes, ...children) {
+export function createElement(type,attributes,...children){
   let element;
-  
-  if (typeof type === 'string') {
-    element = new ElementWrapper(type);  
+  if(typeof type==="string"){
+      element = new ElementWrapper(type);
+  }else{
+      element = new type;
   }
-  else {
-    element = new type;
+  for(let name in attributes){
+      element.setAttribute(name,attributes[name])
   }
-
-  for (let name in attributes) {
-  element.setAttribute(name, attributes[name]);
+  let processChildren = (children) => {
+      for(let child of children){
+          if(typeof child === "object" && (child instanceof Array)){
+              processChildren(child)
+              continue;
+          }
+          if(typeof child === "string"){
+              // 处理文本节点
+              child = new TextWrapper(child);
+          }
+          element.appendChild(child)
+      }
   }
-
-  for (let child of children) {
-  if (typeof child === 'string') {
-    child = new TextWrapper(child);
-  }
-  element.appendChild(child);
-  }
-
+  processChildren(children);
   return element;
 }
 
-export class Component {
+export const STATE = Symbol("state");
+export const ATTRIBUTE = Symbol("attribute");
+export class Component{
   constructor(type) {
+      // this.root = this.render();
+      this[ATTRIBUTE] = Object.create(null);
+      this[STATE] = Object.create(null);
   }
-  setAttribute(name, value) {
-  this.root.setAttribute(name, value);
+  render(){
+      return this.root;
   }
-  appendChild(child) {
-  // this.root.appendChild(child);
-  child.mountTo(this.root);
+  setAttribute(name,value){
+      this[ATTRIBUTE][name] = value;
   }
-  mountTo(parent) {
-  parent.appendChild(this.root);
+  appendChild(child){
+      child.mountTo(this.root)
+  }
+  mountTo(parent){
+      if(!this.root){
+          this.render()
+      }
+      parent.appendChild(this.root);
+  }
+  triggerEvent(type,args){
+      this[ATTRIBUTE]["on" + type.replace(/^[\s\S]/, s => s.toUpperCase())](new CustomEvent(type,{detail: args}));
   }
 }
-
-class ElementWrapper extends Component {
+class ElementWrapper extends Component{
   constructor(type) {
-  this.root = document.createElement(type);
+      super();
+      this.root = document.createElement(type)
+  }
+  setAttribute(name,value){
+      this.root.setAttribute(name,value);
   }
 }
-
-class TextWrapper extends Component {
+// 解析文本节点
+class TextWrapper extends Component{
   constructor(content) {
-  this.root = document.createTextNode(content);
+      super();
+      this.root = document.createTextNode(content)
   }
 }
--- week15/jsx/main.js
+++ week15/jsx/main.js
@@ -1,13 +1,41 @@
 import {Component, createElement} from './framework.js'
 import {Carousel} from './carousel.js'
+import {Button} from './Button.js'
+import {List} from './List.js'
 
 let d = [
-  'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
-  'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
-  'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
-  'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg'
+  {
+    img: 'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
+    url: 'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
+    title: '蓝猫'
+  },
+  {
+    img: 'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
+    url: 'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
+    title: '橘猫'
+  },
+  {
+    img: 'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
+    url: 'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
+    title: '橘猫加白'
+  },
+  {
+    img: 'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg',
+    url: 'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg',
+    title: '喵'
+  }    
 ];
 
 // document.body.appendChild(a);
-let a = <Carousel src={d}></Carousel>
+// let a = <Carousel src={d}></Carousel>
+
+let a = <List data={d}>
+    {(record) =>
+        <div>
+            <img src={record.img} />
+            <a href={record.url}>{record.title}</a>
+        </div>
+    }
+</List>
+
 a.mountTo(document.body);
